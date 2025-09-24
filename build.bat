d21080d4bdd1212ad28ba86077fb890ecda85d5d
Johnny Matthews <johnny.matthews@gmail.com>
2025-09-24 16:36:00 -0500

    update build script

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 SET /P MyVar=< folder.txt
 "%MyVar%\blender.exe" --command extension build
 "c:\Program Files\Pandoc\pandoc.exe" README.md -o README.pdf --pdf-engine="c:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"  --css pdf.css
+move *.zip zips
