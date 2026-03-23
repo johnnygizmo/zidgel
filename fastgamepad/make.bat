af4f21239b50670be6a9f9d41d0908c41239d3fc
Johnny Matthews <johnny.matthews@gmail.com>
2026-03-22 21:50:39 -0500

    Update for Blender 5.1 - Version 1.3.0

--- fastgamepad/make.bat
+++ fastgamepad/make.bat
@@ -1,8 +1,10 @@
- cl /EHsc /LD fastgamepad.cpp ^
+call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+"C:\Program Files\Microsoft Visual Studio\18\Community\VC\Tools\MSVC\14.50.35717\bin\Hostx64\x64\cl.exe" /EHsc /LD fastgamepad.cpp ^
   /std:c++17 ^
-  /I"c:\Program Files\Python311\include" ^
+  /I"c:\Python313\include" ^
   /I"C:\SDL\include" ^
-  /link  /LIBPATH:"c:\Program Files\Python311\libs" python311.lib ^
+  /link  /LIBPATH:"c:\Python313\libs" python313.lib ^
   /LIBPATH:"C:\SDL\VisualC\x64\Release" SDL3.lib
 
 REM copy fastgamepad.dll "C:\Users\johnn\AppData\Roaming\Blender Foundation\Blender\4.5\scripts\addons\fastgamepad.pyd"
--- fastgamepad/maker.bat
+++ fastgamepad/maker.bat
@@ -1,11 +1,11 @@
- cl  /EHsc %1.cpp ^
+ "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Tools\MSVC\14.50.35717\bin\Hostx64\x64\cl.exe"  /EHsc %1.cpp ^
   /std:c++17 ^
-  /I"c:\Program Files\Python311\include" ^
+  /I"c:\Program Files\Python313\include" ^
   /I"C:\SDL\include" ^
-  /link  /LIBPATH:"c:\Program Files\Python311\libs" python311.lib ^
+  /link  /LIBPATH:"c:\Program Files\Python313\libs" python313.lib ^
   /LIBPATH:"C:\SDL\VisualC\x64\Release" SDL3.lib
 
 REM copy fastgamepad.dll "C:\Users\johnn\AppData\Roaming\Blender Foundation\Blender\4.5\scripts\addons\fastgamepad.pyd"
 REM copy controller.py "C:\Users\johnn\AppData\Roaming\Blender Foundation\Blender\4.5\scripts\addons\controller.py"
 copy %1.dll ..\p%1.pyd
-"c:\Program Files\Python311\python.exe" ..\%1.py
+"c:\Program Files\Python313\python.exe" ..\%1.py
